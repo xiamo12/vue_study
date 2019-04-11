ef7279aa6b9f84020deb476f512509ca390e7801
xiamo12 <364023396@qq.com>
2019-04-11 09:43:44 +0800

    vue.js模版语法

--- vue-v-input与v-bind.js
+++ vue-v-input与v-bind.js
@@ -1,6 +1,24 @@
-var vm=new Vue({
-	el:"#app",
+var vm1=new Vue({
+	el:"#comm",
 	data:{
-		use:false,
+		word: "v-text指令",
+		docum: "v-html指令",
+		content: "v-bind绑定事件",
+		class1: "class1",
+		some: "news",
+		title:"title属性绑定",
+	},
+	methods: {
+		warning:function(){
+			alert('dog sir');
+		},
+	},
+});
+
+var vm=new Vue({
+	el:"#root",
+	data:{
+		title:"this is hello world!",
+		content: "this is content",
 	},
 });
