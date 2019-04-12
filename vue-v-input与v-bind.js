ded9aba7166c025abc0570732a13f45db16d30a2
xiamo12 <364023396@qq.com>
2019-04-12 10:11:01 +0800

    vue计算属性和监听器

--- vue-v-input与v-bind.js
+++ vue-v-input与v-bind.js
@@ -15,10 +15,31 @@ var vm1=new Vue({
 	},
 });
 
-var vm=new Vue({
+var vm2=new Vue({
 	el:"#root",
 	data:{
 		title:"this is hello world!",
 		content: "this is content",
+	},
+});
+
+var vm3=new Vue({
+	el:"#temp",
+	data:{
+		firstName: "",
+		lastName: "",
+		count: 0,
+	},
+	// template:"<p>cat and dog</p>",//这里的template将覆盖原有的div子节点
+	computed:{
+//computed属性指的是一个属性由其他属性计算而来。只有当它依赖的属性发生变化的时候才会重新计算。⬆️
+		fullName:function(){
+			return this.firstName + ' ' + this.lastName;
+		},
+	},
+	watch: {//监听器检测数据或属性
+		fullName: function(){
+			this.count ++;
+		},
 	},
 });
